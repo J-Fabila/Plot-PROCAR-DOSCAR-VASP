567d7376aaf9cfacdcdfe3d436424b08b1ccd188
Jorge Fabila <46831682+J-Fabila@users.noreply.github.com>
2021-03-07 08:43:32 -0600

    Update ultimate_version.sh

--- ultimate_version.sh
+++ ultimate_version.sh
@@ -270,7 +270,7 @@ then
       #awk '{print $1}'  atomo8_orbital_d_up.dat  | sort -n | uniq | tr '\n' ' ')
       do
          rm sum_aux_up sum_aux_down
-         for numero in $(seq $initial $final)
+         for numero in $(seq $initial_atom $final_atom)
          do
             grep -- "$energia" atomo${numero}_orbital_${orbital}_up.dat >> sum_aux_up
             total_up=$(awk 'BEGIN {FS="\n"} ; {sum+=$1} END {print sum}' sum_aux_up)
