2c3b4442837bd20e04b59edf29987f4af61ef7be
Jorge Fabila <46831682+J-Fabila@users.noreply.github.com>
2021-03-07 16:41:33 -0600

    Update ultimate_version.sh

--- ultimate_version.sh
+++ ultimate_version.sh
@@ -263,27 +263,30 @@ echo "done"
 
 if [ $group_by = "true" ]
 then
-
    for orbital in $orbitales
    do
       for energia in $(seq -20 0.1 20)
       #awk '{print $1}'  atomo8_orbital_d_up.dat  | sort -n | uniq | tr '\n' ' ')
       do
-         rm sum_aux_up sum_aux_down 2> /dev/null
+         rm sum_aux_up sum_aux_down
          for numero in $(seq $initial_atom $final_atom)
          do
             echo "Calculating Energy:$energia ; Orbital:$orbital Number of atom:$numero ..."
-            grep -- "$energia" atomo${numero}_orbital_${orbital}_up.dat >> sum_aux_up
-            total_up=$(awk 'BEGIN {FS="\n"} ; {sum+=$1} END {print sum}' sum_aux_up)
-            echo " ---> Spin Up"
-            echo "      Done!"
-            grep -- "$energia" atomo${numero}_orbital_${orbital}_down.dat >> sum_aux_down
-            total_down=$(awk 'BEGIN {FS="\n"} ; {sum+=$1} END {print sum}' sum_aux_down)
-            echo " ---> Spin Down"
-            echo "      Done!"
+#cat atomo${numero}_orbital_${orbital}_up.dat | tr '.' ','| grep -w -- "$(echo $en | tr '.' ',' )" | awk '{print $2}' | tr ',' '.' >> sum_aux_up
+            fgrep -wF -- "$energia" atomo${numero}_orbital_${orbital}_up.dat | awk '{print $2}' >> sum_aux_up
+            fgrep -wF -- "$energia" atomo${numero}_orbital_${orbital}_down.dat | awk '{print $2}' >> sum_aux_down
+#            grep -w -- "$energia" atomo${numero}_orbital_${orbital}_down.dat | awk '{print $2}' >> sum_aux_down
          done
-         echo $energia $total_up     >> group_${orbital}_${initial_atom}-${final_atom}_up.dat
-         echo $energia $total_down   >> group_${orbital}_${initial_atom}-${final_atom}_down.dat
+         total_up=$(awk 'BEGIN {FS="\n"} ; {sum+=$1} END {print sum}' sum_aux_up)
+         total_down=$(awk 'BEGIN {FS="\n"} ; {sum+=$1} END {print sum}' sum_aux_down)
+         if [ $(echo $total_up | wc -c ) -gt 1 ]
+         then
+            echo $energia $total_up     >> group_${orbital}_${initial_atom}-${final_atom}_up.dat
+         fi
+         if [ $(echo $total_down | wc -c ) -gt 1 ]
+         then
+            echo $energia $total_down   >> group_${orbital}_${initial_atom}-${final_atom}_down.dat
+         fi
       done
    done
 fi
